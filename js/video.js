abfa7f2710b511fc51a6749ba7a4e4a3770c1298
anwolfe14 <56614581+anwolfe14@users.noreply.github.com>
2021-03-30 16:25:24 -0400

    Add files via upload

---
+++ js/video.js
@@ -0,0 +1,64 @@
+var video = document.getElementById("player1");
+//var video = document.querySelector("player1");
+
+window.addEventListener("load", function() {
+	console.log("Good job opening the window")
+});
+
+document.querySelector("#play").addEventListener("click", function() {
+	 console.log("Play Video")
+	 video.play();
+	 var volume = document.getElementById("volume");
+	 volume.innerHTML = video.volume * 100 + "%";
+ });
+
+ document.querySelector("#pause").addEventListener("click", function() {
+	console.log("Pause Video")
+	video.pause();
+	document.querySelector("#volume").innerHTML = video.volume * 100 + "%";
+});
+
+document.querySelector("#vintage").addEventListener("click", function() {
+	console.log("Vintage")
+	video.classList.add("oldSchool");
+});
+
+document.querySelector("#orig").addEventListener("click", function() {
+	console.log("Back to normal")
+	video.classList.remove("oldSchool");
+});
+document.querySelector("#slider").addEventListener("click", function() {
+	console.log(this)
+	var slider = this.value;
+	video.volume = slider / 100;
+	volume.innerHTML = video.volume * 100 + "%";
+});
+document.querySelector("#slower").addEventListener("click", function() {
+	console.log("Slow Down")
+	video.playbackRate *=.9;
+	console.log(video.playbackRate);
+});
+
+document.querySelector("#faster").addEventListener("click", function() {
+	console.log("Go Faster")
+	video.playbackRate *=.9; //fix, use divison
+	console.log(video.playbackRate)
+});
+document.querySelector("#skip").addEventListener("click", function() {
+	console.log("Skip Ahead")
+	video.playbackRate +=15; //fix, use divison
+	console.log(video.playbackRate)
+});
+document.querySelector("#mute").addEventListener("click", function() {
+	console.log("Muting")
+    if(video.muted == true) {
+		this.innerHTML = "Mute";
+		video.muted = false;
+	}
+	else {
+		this.innerHTML = "Unmuted";
+		video.muted = true;
+	}
+});
+
+//use this https://www.w3schools.com/tags/ref_av_dom.asp
